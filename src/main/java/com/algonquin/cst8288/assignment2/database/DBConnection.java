dea6726bbeb79fad0da473aed7e69eaad4307752
Ryan Xu <ryan.y.xu@hotmail.com>
2024-06-19 15:26:23 -0400

    Use database.properties

--- src/main/java/com/algonquin/cst8288/assignment2/database/DBConnection.java
+++ src/main/java/com/algonquin/cst8288/assignment2/database/DBConnection.java
@@ -3,11 +3,17 @@ package com.algonquin.cst8288.assignment2.database;
 import com.algonquin.cst8288.assignment2.logger.LMSLogger;
 import com.algonquin.cst8288.assignment2.logger.LogLevel;
 import static com.mysql.cj.conf.PropertyKey.logger;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 
 /**
@@ -25,11 +31,37 @@ public class DBConnection {
     private static final LMSLogger logger = LMSLogger.getInstance();
 
     
-    private String serverUrl = "jdbc:mysql://localhost:3306/bookvault";
-    private String userString = "CST8288";
-    private String passwordString = "Assignment2";
+    private String url; // = "jdbc:mysql://localhost:3306/bookvault";
+    private String username; //= "CST8288";
+    private String password; // = "Assignment2";
     private String driverString = "com.mysql.cj.jdbc.Driver";
 //    private String dbName = "bookvault";
+    
+    /**
+     * This function is used to load database properties from database.properties
+     * It equals setter
+     */
+    private void loadDBProp(){
+          Properties props = new Properties();
+        try {
+           // get current directory
+            String currentDirectory = System.getProperty("user.dir"); // It's project directory
+            currentDirectory += "\\src\\main\\java\\com\\algonquin\\cst8288\\assignment2\\database"; // The file's directory
+            // set the abslute path
+            String propFilePath = currentDirectory + File.separator + "database.properties";
+            InputStream in = Files.newInputStream(Paths.get(propFilePath));
+            props.load(in);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        this.url = props.getProperty("jdbc.url");
+        this.username = props.getProperty("jdbc.username");
+        this.password = props.getProperty("jdbc.password");
+
+        System.out.printf("%s\n%s\n%s\n", url, username, password);    
+    }
     
 
     /**
@@ -39,7 +71,8 @@ public class DBConnection {
         String logMessage;
         try {
             Class.forName(driverString);  // Load the MySQL JDBC driver
-            conn = DriverManager.getConnection(serverUrl, userString, passwordString);  // Create the connection
+            loadDBProp();
+            conn = DriverManager.getConnection(url, username, password);  // Create the connection
             logMessage = "Success to connect Database.";
             logger.log(LogLevel.INFO, logMessage); 
             System.out.println(logMessage); 
